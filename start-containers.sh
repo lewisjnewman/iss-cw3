31d80676cc01181bbb32f8b46d0bd6e1a5564ca8
Lewis Newman <lewis.newman@warwick.ac.uk>
2020-03-25 16:07:47 +0000

    Fixed Directory in start script for start mysql container

--- start-containers.sh
+++ start-containers.sh
@@ -7,7 +7,7 @@ docker network create --subnet=192.0.2.0/24 iss-cw3_net
 docker run -d --net iss-cw3_net --ip 192.0.2.3 --hostname database -e MYSQL_ROOT_PASSWORD="test" -e MYSQL_DATABASE="data" --name mysql-server_c mysql-server_i
 
 #populate the mysql database
-docker exec -i mysql-server_c mysql -uroot -p "test" data < sqlconfig/data.sql
+docker exec -i mysql-server_c mysql -uroot -p "test" data < ./mysql/sqlconfig/data.sql
 
 #start the webserver
 docker run -d --net iss-cw3_net --ip 192.0.2.10 --hostname webserver --ad-host database:192.0.2.3 -p 80:80 --name nginx-server_c nginx-server_i

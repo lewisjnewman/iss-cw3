f4f7739349c33fee142961bf7d67b07d016c6485
Lewis Newman <lewis.newman@warwick.ac.uk>
2020-05-13 22:16:58 +0100

    Added no new privileges flag to get seccomp working

--- run-script.sh
+++ run-script.sh
@@ -4,7 +4,7 @@
 docker run -d  -v database_volume:/var/lib/mysql/ --net iss-cw3_net --ip 192.0.2.3 --hostname database --name database_c database_si
 
 #create and run the webserver container
-docker run -d --security-opt seccomp=./nginx-seccomp.json --net iss-cw3_net --ip 192.0.2.10 --hostname webserver --add-host database:192.0.2.3 -p 80:80 --name nginx_server_c nginx_server_si
+docker run -d --security-opt=no-new-privileges --security-opt seccomp=./nginx-seccomp.json --net iss-cw3_net --ip 192.0.2.10 --hostname webserver --add-host database:192.0.2.3 -p 80:80 --name nginx_server_c nginx_server_si
 
 
 
